af8939322987915814938f58765d1760ffcaa6ce
JordanIschard <jordan_is@hotmail.fr>
2019-04-08 11:48:52 +0200

    Maj du main (Zone de test)

--- main.ml
+++ main.ml
@@ -2,20 +2,14 @@ open Lambda.LambdaCalcul ;;
 open LangISWIM.ISWIM;;
 open Cc.CCMachine ;;
 open Scc.SCCMachine ;;
+open Ck.CKMachine ;;
 
 
 (* Test *)
 
-let terme = (App_term(Abs_term("x",Abs_term("y",App_term(Var_term "x", Var_term "y"))),Abs_term("y",App_term(Var_term "y",Var_term "y"))));;
-
-let expression = (App((Abs("w",(App(Var "w",Op("+",[Const 2;Var "w"]))))),Const 2)) ;;
-let expression1 = (App(Abs("x",App(Var "x", Var "y" )),Var "z"));;
-let expression2 = (App(Abs("x",Abs("y",App(Var "x", Var "y"))),Abs("y",App(Var "y",Var "f"))));;
-let expression3 = (App(Abs("z",Abs("b",App(Var "z", Var "b"))),Abs("a",App(Var "a",Var "g"))));;
-let expression4 = (App(Abs("w",Op("-",[App(Var "w",Const 1);Const 5])),App(Abs("x",App(Var "x",Const 10)),Abs("y",Abs("z",Op("+",[Var "z";Var "y"]))))));;
-let expression5 = (App(App(Abs("f",Abs("x",App(Var "f",Var "x"))),Abs("y",Op("+",[Var "y";Var "y"]))),Const 1));;
-
 Printf.printf "Tests des Lambda-calculs \n\n" ;;
+
+let terme = (App_term(Abs_term("x",Abs_term("y",App_term(Var_term "x", Var_term "y"))),Abs_term("y",App_term(Var_term "y",Var_term "y"))));;
 
 afficherTerme terme ;;
 let resT = beta_reduction_term terme;;
@@ -23,6 +17,14 @@ afficherTerme resT ;;
 Printf.printf "\n" ;;
 
 Printf.printf "Tests du langage ISWIM \n\n" ;;
+
+
+let expression = (App((Abs("w",(App(Var "w",Op("+",[Const 2;Var "w"]))))),Const 2)) ;;
+let expression1 = (App(Abs("x",App(Var "x", Var "y" )),Var "z"));;
+let expression2 = (App(Abs("x",Abs("y",App(Var "x", Var "y"))),Abs("y",App(Var "y",Var "f"))));;
+let expression3 = (App(Abs("z",Abs("b",App(Var "z", Var "b"))),Abs("a",App(Var "a",Var "g"))));;
+let expression4 = (App(Abs("w",Op("-",[App(Var "w",Const 1);Const 5])),App(Abs("x",App(Var "x",Const 10)),Abs("y",Abs("z",Op("+",[Var "z";Var "y"]))))));;
+let expression5 = (App(App(Abs("f",Abs("x",App(Var "f",Var "x"))),Abs("y",Op("+",[Var "y";Var "y"]))),Const 1));;
 
 afficherExpr expression ;;
 let res = beta_red expression ;;
@@ -57,4 +59,26 @@ lancerCC expression5 ;;
 Printf.printf "\n" ;;
 
 lancerCC expression4 ;;
+Printf.printf "\n" ;;
+
+Printf.printf "Test de la MachineSCC\n\n" ;;
+
+lancerSCC expression5 ;;
+Printf.printf "\n" ;;
+
+lancerSCC expression4 ;;
+Printf.printf "\n" ;;
+
+
+Printf.printf " Test de la MachineCK\n\n" ;;
+
+let registreCK  = Fun (Abs("x",Var "x"),Arg((Var "y"),(Opd(([(Const 1);(Var "z")],"+"),[],MT)))) ;;
+
+afficherCK (Var "coucou") registreCK ;;
+Printf.printf "\n" ;;
+
+lancerCK expression5 ;;
+Printf.printf "\n" ;;
+
+lancerSCC expression4 ;;
 Printf.printf "\n" ;;
