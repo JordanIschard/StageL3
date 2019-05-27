cff937cf2db7df278cfc7b9b6bdf47900a6082fe
JordanIschard <jordan_is@hotmail.fr>
2019-05-27 15:21:35 +0200

    Historique en cours

---
+++ mainTTSI.ml
@@ -0,0 +1,68 @@
+open Ttsi.MachineTTSI ;;
+open String ;;
+open Printf ;;
+open List ;;
+open LangISWIMTTSI.ISWIM ;;
+
+let expression1 = (App(Abs("w",Op(Sub,[App(Var "w",Const 1);Const 5])),App(Abs("x",App(Var "x",Const 10)),Abs("y",Abs("z",Op(Add,[Var "z";Var "y"]))))));;
+let expression2 = (App(App(Abs("f",Abs("x",App(Var "f",Var "x"))),Abs("y",Op(Add,[Var "y";Var "y"]))),Const 1));;
+
+let expression3 = (App(Abs("x",(
+                              App(
+                                App(
+                                  Spawn_ISWIM(Put_ISWIM("x", 4))
+                                 ,Spawn_ISWIM(Put_ISWIM("x",3))
+                                   )
+                               ,Spawn_ISWIM(App(Wait,Get_ISWIM("x",1,0)))
+                                  )
+                                )),Signal_ISWIM));;
+
+let expression4 = (App(
+                        App(
+                            Abs("s",
+                                Abs("s1",
+                                    App(
+                                        App(
+                                             Spawn_ISWIM(Present_ISWIM("s",Const 6,Const 9))
+                                           , Spawn_ISWIM(Present_ISWIM("s1",Const 3,Const 5))
+                                           )
+                                       , Spawn_ISWIM(Put_ISWIM("s",0))
+                                       )
+                                   )
+                               )
+                          , Signal_ISWIM
+                          )
+                      , Signal_ISWIM
+                      )
+                  );;
+
+let expression5 = (App(
+                        App(
+                             Abs("s"
+                                ,Abs("s1"
+                                    ,App(
+                                          Spawn_ISWIM(Present_ISWIM("s1",Const 1,Op(Add,[Get_ISWIM("s",0,3);Get_ISWIM("s",0,3)])))
+                                         ,Put_ISWIM("s",10)
+                                        )
+                                    )
+                                )
+                            ,Signal_ISWIM
+                           )
+                       ,Signal_ISWIM
+                      )
+                  );;
+
+start expression1 false ;;
+Printf.printf "\n" ;;
+
+start expression2 false;;
+Printf.printf "\n" ;;
+
+start expression3 false;;
+Printf.printf "\n" ;;
+
+start expression4 false;;
+Printf.printf "\n" ;;
+
+start expression5 false;;
+Printf.printf "\n" ;;
